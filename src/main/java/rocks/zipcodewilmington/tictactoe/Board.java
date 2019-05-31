f186502c6f8ae6363f606bb82d23a628f7e1cd07
PrasanthiPenumadu <50220476+PrasanthiPenumadu@users.noreply.github.com>
2019-05-30 20:22:11 -0400

    Update Board.java

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,89 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character dimarray[][] = new Character[3][3];
     public Board(Character[][] matrix) {
+        dimarray = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        Boolean res = false;
+
+        if (dimarray[0][0] == 'X' && dimarray[0][1] == 'X' && dimarray[0][2] == 'X') {
+            res = true;
+
+        }
+        if (dimarray[1][0] == 'X' && dimarray[1][1] == 'X' && dimarray[1][2] == 'X') {
+            res = true;
+        }
+        if (dimarray[2][0] == 'X' && dimarray[2][1] == 'X' && dimarray[2][2] == 'X') {
+            res = true;
+        }
+        if (dimarray[0][0] == 'X' && dimarray[1][1] == 'X' && dimarray[2][2] == 'X') {
+            res = true;
+        }
+        if (dimarray[0][1] == 'X' && dimarray[1][1] == 'X' && dimarray[2][1] == 'X') {
+            res = true;
+        }
+        if (dimarray[0][2] == 'X' && dimarray[1][2] == 'X' && dimarray[2][2] == 'X') {
+            res = true;
+        }
+        if (dimarray[0][0] == 'X' && dimarray[1][0] == 'X' && dimarray[2][0] == 'X') {
+            res = true;
+        }
+        if (dimarray[0][2] == 'X' && dimarray[1][1] == 'X' && dimarray[2][0] == 'X') {
+            res = true;
+        }
+        return res;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        Boolean res = false;
+        if (dimarray[0][0] == 'O' && dimarray[1][1] == 'O' && dimarray[2][2] == 'O') {
+            res = true;
+        }
+        if (dimarray[0][0] == 'O' && dimarray[1][0] == 'O' && dimarray[2][0] == 'O') {
+            res = true;
+        }
+        if (dimarray[0][1] == 'O' && dimarray[1][1] == 'O' && dimarray[2][1] == 'O') {
+            res = true;
+        }
+        if (dimarray[0][2] == 'O' && dimarray[1][2] == 'O' && dimarray[2][2] == 'O') {
+            res = true;
+        }
+        if (dimarray[0][2] == 'O' && dimarray[1][1] == 'O' && dimarray[2][0] == 'O') {
+            res = true;
+        }
+        if (dimarray[0][0] == 'O' && dimarray[0][1] == 'O' && dimarray[0][2] == 'O') {
+            res = true;
+        }
+        if (dimarray[1][0] == 'O' && dimarray[1][1] == 'O' && dimarray[1][2] == 'O') {
+            res = true;
+        }
+        if (dimarray[2][0] == 'O' && dimarray[2][1] == 'O' && dimarray[2][2] == 'O') {
+            res = true;
+        }
+        return res;
+
     }
 
     public Boolean isTie() {
-        return null;
+
+
+        if (isInFavorOfO() == isInFavorOfX())
+            return true;
+        else
+            return false;
+
     }
 
     public String getWinner() {
-        return null;
+       if(isInFavorOfX()==true)
+           return "X";
+       else if(isInFavorOfO()==true)
+           return "O";
+       else
+           return "";
     }
-
 }
